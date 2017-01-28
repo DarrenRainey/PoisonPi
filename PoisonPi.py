236cc5f2c87e9e81353ed9c3a70a0f0a425fa73c
Darren Rainey <darren@darrenraineys.co.uk>
2017-01-28 18:40:37 +0000

    Update PoisonPi.py

--- PoisonPi.py
+++ PoisonPi.py
@@ -8,7 +8,7 @@ while True:
 
 		# Routersploit Framework
 		if userInput in ['rsf', 'RSF', 'Routersploit', 'RoutersploitFramework', 'RouterSploit', 'routersploit']:
-      os.system("python $HOME/Attack-Toolkit/routersploit/rsf.py")
+   		   os.system("python $HOME/Attack-Toolkit/routersploit/rsf.py")
 
 		# Exit Function
 		elif userInput in ['exit', 'Exit', 'quit', 'Quit']:
@@ -17,13 +17,12 @@ while True:
 
 		# Help/Guide Function
 		elif userInput in ['HELP', 'Help', 'help']:
-      print("PosionPi Menu By Darren Rainey")
-      print("Exit     -       Exit Toolkit")
-      print("RSF      -       Routersploit Framework")
-      print("Update   -       Get The Latest Version")
+   		   print("PosionPi Menu By Darren Rainey")
+   		   print("Exit     -       Exit Toolkit")
+  		   print("RSF      -       Routersploit Framework")
+  		   print("Update   -       Get The Latest Version")
 		
 		# IDA0005 Error Function
 		else:
 			Error = ['What was that ?', 'I"m sorry I did not understand you', 'Excuse me, What was that ?', 'Please say that again im not sure what you mean']
 			print (Error[int(random.random() * len(Error))])
-			#print("I did not understand what you said")
