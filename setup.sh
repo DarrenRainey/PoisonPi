e2313a30036d72c34d4c0dae0a2bf2acca4f631f
Darren Rainey <darren@darrenraineys.co.uk>
2017-01-29 03:19:45 +0000

    Added Wifite2

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ sudo apt-get install python-dev python-pip libncurses5-dev git --yes
 git clone https://github.com/reverse-shell/routersploit
 cd routersploit
 pip install -r requirements.txt
+cd ..
+# Wifite 2 Dependinces
+sudo apt-get install aircrack-ng
+# Get Wifite2
+git clone https://github.com/derv82/wifite2
