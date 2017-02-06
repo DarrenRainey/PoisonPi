00f8b0c1d53a18ea6504393b2bfca58f1f46e099
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-06 17:56:56 +0000

    Added Function To Update Current Repository's

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 mkdir $HOME/Attack-Toolkit
 cd $HOME/Attack-Toolkit
 
+# Update Repository
+sudo apt-get update
+
 # Routersploit Dependinces
 sudo apt-get install python-dev python-pip libncurses5-dev git --yes
 # Get Routersploit
